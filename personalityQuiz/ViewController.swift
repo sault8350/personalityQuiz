4607f78dcc8b0ab1db6aee9f5ef559b3b0e2e587
Samuel Ault <sault8350@jh-mba158-01.d214.org>
2018-02-28 11:20:19 -0600

    Initial Commit

---
+++ personalityQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  personalityQuiz
+//
+//  Created by Samuel Ault on 2/28/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
