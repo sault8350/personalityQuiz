97c84e41c212d8de7845dbcdb288f978bf1c0fee
Samuel Ault <sault8350@jh-mba158-01.d214.org>
2018-03-06 11:23:47 -0600

    added controls to questions view controller

--- QuestionsViewController.swift
+++ QuestionsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Results_ViewController: UIViewController {
+class QuestionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- ResultsViewController.swift
+++ ResultsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Questions_ViewController: UIViewController {
+class ResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
